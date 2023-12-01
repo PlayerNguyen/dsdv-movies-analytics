321bd06f09e5ceee01cc99e37c845ef3ed078263
PlayerNguyen <daudaua3782@gmail.com>
2023-12-01 13:49:00 +0700

    feat: add code base

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function App() {
+  return (
+    <div>
+      <p>Code react sẽ ở đây</p>
+      <Button>Nhấn để không làm gì</Button>
+    </div>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const app = document.getElementById("app");
+
+if (app !== null) {
+  createRoot(app).render(<App />);
+}
