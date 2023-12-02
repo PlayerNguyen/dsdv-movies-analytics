6f8d5755f361f9a2ac285508bcbb4624b3117d73
mtnguyenquynh <ITITIU20309@student.hcmiu.edu.vn>
2023-12-02 11:45:06 +0700

    02/12/2023: Update option in Category

--- src/views/category/Category.js
+++ src/views/category/Category.js
@@ -8,18 +8,32 @@ import CIcon from '@coreui/icons-react'
 // import WidgetsDropdown from '../widgets/WidgetsDropdown'
 
 const Category = () => {
+  const options = [
+    'Genre',
+    { label: 'Ranking', value: 'rank' },
+    { label: 'Year', value: 'year' },
+    { label: 'Alphabetical', value: 'alphabetical' },
+  ]
+
+  options.sort((a, b) => {
+    if (typeof a === 'string') {
+      return a.localeCompare(b)
+    } else {
+      return a.label.localeCompare(b.label)
+    }
+  })
+
   return (
-    <>
-      <CFormSelect
-        aria-label="Default select example Category"
-        options={[
-          'Genre',
-          { label: 'Drama', value: 'drama' },
-          { label: 'Crime, Drama', value: 'Crime, Drama' },
-          { label: 'Others', value: 'others' },
-        ]}
-      />
-    </>
+    <div>
+      <span>Filter by: </span>
+      <select aria-label="Default select example Category">
+        {options.map((option, index) => (
+          <option key={index} value={typeof option === 'string' ? option : option.value}>
+            {typeof option === 'string' ? option : option.label}
+          </option>
+        ))}
+      </select>
+    </div>
   )
 }
 
